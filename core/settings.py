194410458c9ce85f9fdd59f202db14e3f82f0937
batuhanaltog <batuhanaltog88@gmail.com>
2023-04-10 00:10:54 +0300

    removing 1 from localhost

--- core/settings.py
+++ core/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DJANGO_DEBUG")
 
-ALLOWED_HOSTS = ["localhost1","127.0.0.1","taskmateapp.up.railway.app"]
+ALLOWED_HOSTS = ["localhost","127.0.0.1","taskmateapp.up.railway.app"]
 CSRF_TRUSTED_ORIGINS = ['https://taskmateapp.up.railway.app']
 
 
